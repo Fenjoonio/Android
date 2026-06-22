f9271cd6b22af16dec1e79d5a531a8ee813c4a85
Yasin Silavi <ys.silavi@gmail.com>
2026-06-22 13:00:26 +0330

    chore: update project version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "io.fenjoon.app"
         minSdk = 24
         targetSdk = 36
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 23
+        versionName = "0.2.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
