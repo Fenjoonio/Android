77d4d8816788abfbf01a8bf09db2acef4ccd5205
Yasin Silavi <ys.silavi@gmail.com>
2026-08-04 02:31:10 +0330

    chore: bump version to `0.2.1`

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "io.fenjoon.app"
         minSdk = 24
         targetSdk = 36
-        versionCode = 23
-        versionName = "0.2.0"
+        versionCode = 24
+        versionName = "0.2.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
