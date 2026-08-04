9fdc032980a9cee84e61b967725845fffa19f82b
Yasin Silavi <ys.silavi@gmail.com>
2026-08-04 20:35:34 +0330

    fix: add extra padding on the splash screen

--- app/src/main/java/io/fenjoon/app/MainActivity.kt
+++ app/src/main/java/io/fenjoon/app/MainActivity.kt
@@ -46,6 +46,7 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
@@ -864,6 +865,7 @@ private fun FenjoonSplash(modifier: Modifier = Modifier) {
             horizontalAlignment = Alignment.CenterHorizontally,
             modifier = Modifier
                 .align(Alignment.BottomCenter)
+                .navigationBarsPadding()
                 .padding(bottom = 24.dp)
         ) {
             Text(
